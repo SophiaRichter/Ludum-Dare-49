3f13a510e12907fb8b76cccf67b3b7c64b85bc12
Sophia <s.richter171@gmx.de>
2021-10-02 14:08:41 +0200

    Main character animations embedded

--- Assets/Code/PlayerScript.cs
+++ Assets/Code/PlayerScript.cs
@@ -6,22 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float jumpHeight = 5f;
+    bool isIdle = true;
+    bool isWalking = false;
+    bool isWalkingDown = false;
+    bool isWalkingUp = false;
+    
     float velocity = 2f;
-    bool onGround = true;
-    bool doubleJump = true;
-    private float levelStart;
 
     private Transform blob;
 
-    Rigidbody2D rigidbody;
+    private Rigidbody2D rigidbody;
+    private Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        levelStart = Time.time;
-        blob = transform.Find("blob");
+        blob = transform.Find("Blob");
+        anim = blob.GetComponent<Animator>(); 
+
     }
 
     // Update is called once per frame
@@ -29,22 +33,101 @@ public class PlayerScript : MonoBehaviour
     {
         float xDir = Input.GetAxisRaw("Horizontal");
         float yDir = Input.GetAxisRaw("Vertical");
-        move(velocity, xDir, yDir);
+        setDirections(xDir, yDir);
 
+        move(velocity, xDir, yDir);
+        animate();
+
+    }
+
+    private void setDirections(float xDir, float yDir)
+    {
+        if (xDir == 0 && yDir == 0)
+        {
+            isIdle = true;
+            isWalking = false;
+            isWalkingDown = false;
+            isWalkingUp = false;
+            return;
+        }
+        if (xDir == 0 && yDir < 0)
+        {
+            isIdle = false;
+            isWalkingDown = true;
+            isWalking = false;
+            isWalkingUp = false;
+            return;
+        }
+        if (xDir == 0 && yDir > 0)
+        {
+            isIdle = false;
+            isWalkingUp = true;
+            isWalking = false;
+            isWalkingDown = false;
+            return;
+        }
+        if (xDir != 0)
+        {
+            isIdle = false;
+            isWalking = true;
+            isWalkingDown = false;
+            isWalkingUp = false;
+        }
+    }
+
+    private void animate()
+    {
+        //AnimatorClipInfo[] clips = anim.GetCurrentAnimatorClipInfo(0);
+
+        
+        //anim.Play("Blob_Idle");
+        
+        if (isIdle)
+        {
+            anim.Play("Blob_Idle");
+            return;
+        }
+
+        if (isWalking)
+        {
+            anim.Play("Blob_Walking");
+            return;
+        }
+
+        if (isWalkingDown)
+        {
+            anim.Play("Blob_WalkingDown");
+            return;
+        }
+
+        if (isWalkingUp)
+        {
+            anim.Play("Blob_WalkingUp");
+            return;
+        }
     }
 
     private void move(float vel, float xDir, float yDir)
     {
-        rigidbody.velocity = new Vector2(vel * xDir, vel * yDir);
-        
+        rigidbody.velocity = new Vector2(vel * xDir, vel * yDir);       
+
         //let model face walking direction
         if (xDir > 0)
         {
-            //transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+            blob.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
         }
         else if (xDir < 0 )
         {
-            //transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
+            blob.transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
+        }
+
+        if (yDir < 0)
+        {
+            //blob.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+        }
+        else if (yDir > 0)
+        {
+            //blob.transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
         }
     }
 
@@ -52,7 +135,6 @@ public class PlayerScript : MonoBehaviour
     {
         if (other.gameObject.tag == "Collider")
         {
-            onGround = true;
         }
     }
 
